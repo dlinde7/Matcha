10d4842b58e9e0c44d75db14fc1ca27f64abe5d7
Gilbert Fernandes <gfernand@student.wethinkcode.co.za>
2020-01-09 07:23:14 +0200

    Update mongo.js

--- gfernand/mongo.js
+++ gfernand/mongo.js
@@ -22,5 +22,21 @@ var createColl = function () {
 	});
 };
 
+var regUser = function () {
+	MongoClient.connect(url, function (err, db){
+		if (err) throw err;
+		var dbo = db.db("matcha");
+		var userInfo = { name: name, lastname: lname, gender: gen, hair: hair, sexualorientation: sor,
+			personality: { 1: , 2: , 3: }, eyecolour: eye, height: len, age: age, race: rc, 
+			features: { 1: , 2: , 3: }, personalitypref: { 1: , 2: , 3: }, profilepic: pric, verified: 0 };
+		dbo.collection("users").insertOne(userInfo, function(err, res) {
+			if (err) throw err;
+			console.log("1 document inserted");
+			db.close();
+		});
+	});
+};
+
 module.exports = dbConn
 module.exports = createColl
+module.exports = regUser
