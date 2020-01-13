9f14680d2a56785da07bb93f6417b6f2537f1876
Gilbert FERNANDES <gfernand@c5r10s5.wethinkcode.co.za>
2020-01-13 14:30:56 +0200

    more functions

--- gfernand/mongo.js
+++ gfernand/mongo.js
@@ -1,17 +1,17 @@
-var MongoClient = require('mongodb').MongoClient;
+var mongoClient = require('mongodb').mongoClient;
 var url = "mongodb://localhost:27017/matcha";
 
 var dbConn = function () {
-	MongoClient.connect(url, function (err, db) {
+	mongoClient.connect(url, function (err, db) {
 		if (err) throw err;
-		console.log("Database created!");
+		console.log("Database connected!");
 		db.close();
 	});
 };
 
 
 var createColl = function () {
-	MongoClient.connect(url, function (err, db) {
+	mongoClient.connect(url, function (err, db) {
 		if (err) throw err;
 		var dbo = db.db("matcha");
 		dbo.createCollection("users", function (err, res) {
@@ -23,20 +23,94 @@ var createColl = function () {
 };
 
 var regUser = function () {
-	MongoClient.connect(url, function (err, db){
+	mongoClient.connect(url, function (err, db){
 		if (err) throw err;
 		var dbo = db.db("matcha");
-		var userInfo = { name: name, lastname: lname, gender: gen, hair: hair, sexualorientation: sor,
-			personality: { 1: , 2: , 3: }, eyecolour: eye, height: len, age: age, race: rc, 
-			features: { 1: , 2: , 3: }, personalitypref: { 1: , 2: , 3: , 4: , 5: , 6: , 7: , 8: , 9: , 10: }, profilepic: pric, verified: 0 };
+		var userInfo = { username: user, email: email, password: pwd, verified: 0 };
 		dbo.collection("users").insertOne(userInfo, function(err, res) {
 			if (err) throw err;
-			console.log("1 document inserted");
+			console.log("New user added");
 			db.close();
 		});
 	});
 };
 
+var verify = function () {
+	mongoClient.connect(url, function (err, db){
+		if (err) throw err;
+		var dbo = db.db("matcha");
+		var myQuery = { username: user};
+		var newValues = {verified: 1};
+		dbo.collection("users").updateOne(myQuery, newValues, function(err, res) {
+			if (err) throw err;
+			console.log("User has been verified");
+			db.close;
+		});
+	});
+}
+
+var accSetUp = function () {
+	mongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("matcha");
+		var myQuery = { username: user, verified: 1};
+		var newValues = { $set: { name: name, lastname: lname, gender: gen, hair: hair, sexualorientation: sor,
+			personality: { 1: , 2: , 3: }, eyecolour: eye, height: len, age: age, race: rc, features: { 1: , 2: , 3: },
+			personalitypref: { 1: , 2: , 3: , 4: , 5: , 6: , 7: , 8: , 9: , 10: }, profilepic: pric } };
+		dbo.collection("users").updateOne(myQuery, newValues, function(err, res) {
+			if (err) throw err;
+			console.log("profile updated");
+			db.close();
+		});
+	});
+}
+
+var newPassword = function () {
+	mongoClient.connect(url, function (err, db){
+		if (err) throw err;
+		var dbo = db.db("matcha");
+		var myQuery = { username: user};
+		var newValues = { password: pwd};
+		dbo.collection("users").updateOne(myQuery, newValues, function(err, res) {
+			if (err) throw err;
+			console.log("Password updated");
+			db.close;
+		});
+	});
+}
+
+var newEmail = function () {
+	mongoClient.connect(url, function (err, db){
+		if (err) throw err;
+		var dbo = db.db("matcha");
+		var myQuery = { username: user};
+		var newValues = { email: email};
+		dbo.collection("users").updateOne(myQuery, newValues, function(err, res) {
+			if (err) throw err;
+			console.log("Email updated");
+			db.close;
+		});
+	});
+}
+
+var deleteUser = function () {
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("matcha");
+		var myquery = { username: user };
+		dbo.collection("users").deleteOne(myquery, function(err, obj) {
+			if (err) throw err;
+			console.log("User account deleted!");
+			db.close();
+		});
+	});
+}
+
 module.exports = dbConn
 module.exports = createColl
 module.exports = regUser
+module.exports = accSetUp
+module.exports = verify
+module.exports = newPassword
+module.exports = newEmail
+module.exports = deleteUser
